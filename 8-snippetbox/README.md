5d867415c65ba27202e8e7f4ffa28ca240fb09d9
Marlon Marcello <marlon.marcello@gmail.com>
2024-12-15 13:43:10 -0800

    feature(snippetbox): improves security with tls and more

--- 8-snippetbox/README.md
+++ 8-snippetbox/README.md
@@ -1,3 +1,32 @@
 # Let's Go!
 
 Course work from the awesome, and HIGHLY recommended, [Let's Go](https://lets-go.alexedwards.net/) book.
+
+## Notes
+
+### Generating a TLS certificate.
+
+Create a directory at the root of the project to store the keys:
+```sh
+mkdir tls & cd tls
+```
+
+Find out where Go is installed:
+```sh
+which go
+```
+
+It should yield something like this, take note of this path:
+```
+/usr/local/go-1.23/bin/go
+```
+
+Use it to call `generate_cert.go`:
+
+```sh
+go run /usr/local/go-1.23/bin/go/src/crypto/tls/generate_cert.go --rsa-bits=2048 --host=localhost
+```
+
+It should generate a pair of `cert.pem` and `key.pem` files.
+
+That's it.
--- 8-snippetbox/cmd/web/main.go
+++ 8-snippetbox/cmd/web/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
 	"flag"
 	"log/slog"
 	"net/http"
@@ -85,6 +86,8 @@ func main() {
 	sessionManager := scs.New()
 	sessionManager.Store = pgxstore.New(db)
 	sessionManager.Lifetime = 12 * time.Hour
+	// This ensures that cookies are only sent over HTTPS
+	sessionManager.Cookie.Secure = true
 
 	// defer the db connection close so it runs before the end of main()
 	defer db.Close()
@@ -107,6 +110,29 @@ func main() {
 		sessionManager: sessionManager,
 	}
 
+	tlsConfig := &tls.Config{
+		// Go supports a few elliptic curves, but as of Go 1.23 only tls.CurveP256 and tls.X25519 have assembly implementations. The others are very CPU intensive, so omitting them helps ensure that our server will remain performant under heavy loads.
+		CurvePreferences: []tls.CurveID{tls.X25519, tls.CurveP256},
+
+		/*
+		   Lesson 9.05 explains more about cypher suites and how Go utilizes them with TLS connections.
+		   For backwards compatibility reasons, to support TLS1.0-1.2, Go does allow and uses by default some older cypher suites that are vulnerable to attacks
+		   All cyphers used by Go for TLS1.3 are safe by default and always used
+		   This list is a list of cypher suites that work with TLS1.2 and are considered safe.
+		   Reccomended configuration: https://wiki.mozilla.org/Security/Server_Side_TLS
+
+		   Marlon's opinion: The drawback here is that we are excluding TLS1.0 but if a user has such an older browser, they shouldn't be on the internet.
+		*/
+		CipherSuites: []uint16{
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
+		},
+	}
+
 	srv := &http.Server{
 		Addr:    *addr,
 		Handler: app.routes(),
@@ -115,7 +141,17 @@ func main() {
 
 			By default, it writes these entries using the standard logger — which means they will be written to the standard error stream (instead of standard out like our other log entries), and they won’t be in the same format as the rest of our nice structured log entries.
 		*/
-		ErrorLog: slog.NewLogLogger(logger.Handler(), slog.LevelError),
+		ErrorLog:  slog.NewLogLogger(logger.Handler(), loggerLevel),
+		TLSConfig: tlsConfig,
+		/*
+		  Timeouts to improve resiliency
+		  All three of these timeouts — IdleTimeout, ReadTimeout and WriteTimeout — are server-wide settings which act on the underlying connection and apply to all requests irrespective of their handler or URL.
+
+		  There are really good explanations for all of these on Lesson 9.06
+		*/
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
 	}
 
 	logger.Info("starting server", "addr", srv.Addr)
@@ -127,7 +163,9 @@ func main() {
 
 	   https://fideloper.com/golang-http-handlers
 	*/
-	err = srv.ListenAndServe()
+
+	// [NOTE] Using HTTPS - Go’s will automatically upgrade the connection to use HTTP/2 if the client supports it.
+	err = srv.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
 	logger.Error(err.Error())
 	os.Exit(1)
 }
